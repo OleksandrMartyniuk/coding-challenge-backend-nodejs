d7d126db5a4ef328b1b7de9f0cd1842c1d073681
OleksandrMartyniuk <alexandrmartiniuk@gmail.com>
2019-03-06 13:55:24 +0200

    fix wrap error message generating

--- src/util/wrap.ts
+++ src/util/wrap.ts
@@ -8,9 +8,8 @@ interface WrapOptions {
 export function wrap(handler: Handler, options?: WrapOptions): Handler {
     return async function (req: Request, res: Response, next: NextFunction) {
         return Promise.resolve(handler(req, res, next)).catch(err => {
-            console.error(err);
             const originalMessage = err.original && err.original.message ? err.original.message : null;
-            const message = [err.message, originalMessage].join(': ');
+            const message = [err.message, originalMessage].filter(Boolean).join(': ');
             if (options) {
                 next({
                     status: options.forceStatus || err.status,
