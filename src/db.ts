1ca20acf697454b6d735af3472b9869152e8a75f
OleksandrMartyniuk <alexandrmartiniuk@gmail.com>
2019-03-07 16:35:20 +0200

    env fixes

--- src/db.ts
+++ src/db.ts
@@ -6,9 +6,9 @@ export async function initDatabaseConnection() {
         dialect: 'mysql',
         port: parseInt(process.env['DB_PORT']) || 3306,
         database: process.env['DB_DATABASE'],
-        username: process.env['DB_USER'].toString(),
-        password: process.env['DB_PASSWORD'].toString(),
-        host: process.env['DB_HOST'].toString(),
+        username: process.env['DB_USER'],
+        password: process.env['DB_PASSWORD'],
+        host: process.env['DB_HOST'],
         logging: false,
         operatorsAliases: false,
         modelPaths: [__dirname + '/model']
